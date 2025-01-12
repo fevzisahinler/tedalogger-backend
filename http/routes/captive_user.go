b23a287e64abe1535535d9d203a30b1399cc123d
Fevzi Can Sahinler <fevzisahinler@gmail.com>
2025-01-12 20:52:06 +0300

    fixed header problem

--- http/routes/captive_user.go
+++ http/routes/captive_user.go
@@ -9,19 +9,16 @@ import (
 )
 
 func CaptiveUserRoutes(app *fiber.App) {
-	protected := app.Group("/captive_user", middleware.JWTMiddleware())
 
-	protected.Post("/create", controllers.CreateCaptiveUser)
-	protected.Get("/get/:id", controllers.GetCaptiveUser)
-	protected.Put("/update/:id", controllers.UpdateCaptiveUser)
-	protected.Delete("/delete/:id", controllers.DeleteCaptiveUser)
-	protected.Get("/get-all", controllers.GetAllCaptiveUsers)
+	app.Post("/captive_user/create", middleware.JWTMiddleware(), controllers.CreateCaptiveUser)
+	app.Get("/captive_user/get/:id", middleware.JWTMiddleware(), controllers.GetCaptiveUser)
+	app.Put("/captive_user/update/:id", middleware.JWTMiddleware(), controllers.UpdateCaptiveUser)
+	app.Delete("/captive_user/delete/:id", middleware.JWTMiddleware(), controllers.DeleteCaptiveUser)
+	app.Get("/captive_user/get-all", middleware.JWTMiddleware(), controllers.GetAllCaptiveUsers)
 
-	captiveUser := app.Group("/captive_user")
-
-	captiveUser.Post("/register", controllers.RegisterCaptiveUser)
-	captiveUser.Post("/verify_otp", controllers.VerifyOTP)
-	captiveUser.Post("/login", controllers.LoginCaptiveUser)
-	captiveUser.Post("verify_login_otp", controllers.VerifyLoginOTP)
-	captiveUser.Post("/resend_otp", controllers.ResendOTP)
+	app.Post("/captive_user/register", controllers.RegisterCaptiveUser)
+	app.Post("/captive_user/verify_otp", controllers.VerifyOTP)
+	app.Post("/captive_user/login", controllers.LoginCaptiveUser)
+	app.Post("/captive_user/verify_login_otp", controllers.VerifyLoginOTP)
+	app.Post("/captive_user/resend_otp", controllers.ResendOTP)
 }
